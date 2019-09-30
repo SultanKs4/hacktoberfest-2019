33ad9bbc57f8dbd720857ebbcf135197e9a0de64
SultanKs4 <sultangendut@gmail.com>
2019-09-30 22:04:02 +0700

    add full name

--- hw.rb
+++ hw.rb
@@ -2,8 +2,15 @@ class Sample
     def hello
         puts "Hello World!"
     end
+    def name
+        fistname = "Sultan"
+        lastname = "Achmad Qum Masykuro NS"
+        fullname = fistname + " " + lastname
+        puts fullname
+    end
 end
 
 object = Sample.new
 
 object.hello
+object.name
